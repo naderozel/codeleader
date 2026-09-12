750db1a9ddf6852677eb24d0f4d3098716a1026b
naderozel <34371437+naderozel@users.noreply.github.com>
2026-09-12 20:55:37 +0300

    Refactor app.py to include updated academy info

--- app.py
+++ app.py
@@ -87,10 +87,10 @@ SYSTEM_PROMPT = f"""
 """
 
 Groq_model = "openai/gpt-oss-120b"
-
 @st.cache_resource
 def get_client():
-    return Groq(api_key="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"")
+    return Groq(api_key=st.secrets["GROQ_API_KEY"])
+
 
 
 client = get_client()
